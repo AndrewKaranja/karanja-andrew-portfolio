fafe9977c76ec851bb9c7b8c31ccc23ef38de6de
AndrewKaranja <andywanji56@gmail.com>
2022-02-02 16:57:50 +0300

    Personal portfolio

---
+++ src/components/Header.jsx
@@ -0,0 +1,180 @@
+import React, { Component, createRef } from "react";
+import { IconContext } from "react-icons";
+import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
+import { AiFillLinkedin, AiOutlineGithub } from "react-icons/ai";
+// import MagnetMouse from "./utils/magnentMouse";
+import { Link } from "react-scroll";
+import logo from '../assets/images/logobb.png';
+
+// Or Access Link,Element,etc as follows
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.navbar = createRef();
+    this.menu = createRef();
+  }
+  state = {
+    links: [
+      { id: 1, name: "Home", to: "home", class: "" },
+      { id: 2, name: "About", to: "about", class: "" },
+      { id: 3, name: "Work", to: "work", class: "" },
+      { id: 4, name: "Portfolio", to: "portfolio", class: "" },
+      { id: 5, name: "contact", to: "contact", class: "" },
+    ],
+  };
+  componentDidMount() {
+    // const cursor1 = document.querySelector(".cursor1");
+    // const cursor2 = document.querySelector(".cursor2");
+    // window.addEventListener("mousemove", (e) => {
+    //   cursor1.style.cssText = cursor2.style.cssText =
+    //     "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+    // });
+    // let mm = new MagnetMouse({
+    //   magnet: {
+    //     element: ".hamburger-area",
+    //   },
+    // });
+    // mm.init();
+  }
+
+  handleShowLinks = () => {
+    this.navbar.current.classList.toggle("open");
+    this.menu.current.classList.toggle("open");
+    // document.querySelector(".cursor1").classList.toggle("open");
+    // document.querySelector(".cursor2").classList.toggle("open");
+  };
+  handleHover = (link) => {
+    const links = this.state.links.map((item) => {
+      if (item.id === link.id) {
+        item.class = "focusOut focused";
+      } else {
+        item.class = "focusOut";
+      }
+      return item;
+    });
+    this.setState({ links });
+  };
+  handleHoverOut = () => {
+    const links = this.state.links.map((item) => {
+      item.class = "";
+
+      return item;
+    });
+    this.setState({ links });
+  };
+  handleCursorEnter = () => {};
+  closeMenu = () => {
+    this.navbar.current.classList.remove("open");
+    this.menu.current.classList.remove("open");
+  };
+
+  render() {
+    return (
+      <header>
+        <nav className="header-main-container" ref={this.menu}>
+          <div className="logo-container">
+            <Link
+              to="home"
+              spy={true}
+              smooth={true}
+              duration={500}
+              delay={800}
+              className="logo-area"
+              onClick={this.closeMenu}
+            >
+            <img src={logo} alt='' className="logo-main"/>
+
+             
+            </Link>
+          </div>
+          <div className="hamburger-container">
+            <div className="hamburger-area" onClick={this.handleShowLinks}>
+              <div className="burger"></div>
+              <div className="burger"></div>
+              <div className="burger"></div>
+            </div>
+          </div>
+        </nav>
+        <nav className="header-links-main-container" ref={this.navbar}>
+          <div className="navbar-overlay-main-container"></div>
+          <div className="links-wrapper">
+            <div className="menu-text">
+              <h1>Menu</h1>
+            </div>
+            <ul>
+              {this.state.links.map((link, index) => (
+                <li
+                  onMouseEnter={() => this.handleHover(link)}
+                  onMouseLeave={() => this.handleHoverOut(link)}
+                  className={link.class}
+                  key={link.id}
+                >
+                  <Link
+                    to={link.to}
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    delay={800}
+                    key={link.id}
+                    onClick={this.handleShowLinks}
+                  >
+                    {link.name}
+                  </Link>
+                  <div className="link-number">0{index + 1}</div>
+                  <div className="link-text-overlay">{link.name}</div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="contact-details">
+            <div className="contact-text">
+              <p>andrewkaranja007@gmail.com</p> <br />
+              <p>+(254) 711261436</p> <br />
+              <span>Nairobi,Kenya</span>
+            </div>
+            <div className="contact-details-icon-wrapper">
+              <div className="contact-icon-item">
+                <a href="https://twitter.com/karanjah_1" target="blank">
+                  <IconContext.Provider
+                    value={{ className: "header-links-icon" }}
+                  >
+                    <FaTwitter />
+                  </IconContext.Provider>
+                </a>
+                <a
+                  href="https://www.instagram.com/karanja.a.k/"
+                  target="blank"
+                >
+                  <IconContext.Provider
+                    value={{ className: "header-links-icon" }}
+                  >
+                    <FaInstagram />
+                  </IconContext.Provider>
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/andrew-karanja-388316170/"
+                  target="blank"
+                >
+                  <IconContext.Provider
+                    value={{ className: "header-links-icon" }}
+                  >
+                    <AiFillLinkedin />
+                  </IconContext.Provider>
+                </a>
+                <a href="https://github.com/AndrewKaranja" target="blank">
+                  <IconContext.Provider
+                    value={{ className: "header-links-icon" }}
+                  >
+                    <AiOutlineGithub />
+                  </IconContext.Provider>
+                </a>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default Header;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,133 @@
+import React, { Component } from "react";
+import ProjectCard from "./common/ProjectCard";
+import project1 from "../assets/images/projects/kinga.png";
+import project2 from "../assets/images/projects/expensetracker.png";
+import project3 from "../assets/images/projects/kplayer.png";
+
+
+export default class Portfolio extends Component {
+  state = {
+    shownTab: "all",
+  };
+
+  handleHowTab = (tab) => {
+    this.setState({ shownTab: tab });
+  };
+
+  render() {
+    const { shownTab } = this.state;
+    const projects = [
+      {
+        id: 2,
+        image: project1,
+        title: "Kinga Sacco",
+        description:
+          "Kinga sacco is a sacco website meant for members of PSKenya.The society's core business is mobilize savings from the members out of which credit is approved at affordable interest rates.Some of the features in the website include a loan calculator,members forum,information center,chatbot and a membership registration system",
+        demoLink: "https://www.kingasacco.co.ke/",
+        sourceCodeLink: "https://www.kingasacco.co.ke/",
+        technologies: [
+          { id: 1, name: "Javascript" },
+          { id: 2, name: "WordPress" },
+        ],
+        category_id: 1,
+        animation: "zoom-in-up",
+      },
+      {
+        id: 1,
+        image: project2,
+        title: "Expense Tracker",
+        description:
+          "This is an expense tracker that allows users to calculate and keep track of their expenses",
+        demoLink: "https://github.com/AndrewKaranja/Expense-Tracker",
+        sourceCodeLink: "https://github.com/AndrewKaranja/Expense-Tracker",
+        technologies: [
+          { id: 1, name: "Javascript" },
+          { id: 2, name: "ReactJs" },
+          { id: 3, name: "Node js" },
+        ],
+        category_id: 1,
+        animation: "zoom-in-up",
+      },
+
+      {
+        id: 3,
+        image: project3,
+        title: "K player",
+        description:
+          "Minimalist music player created for android.   ",
+        demoLink: "https://github.com/AndrewKaranja/K-Player",
+        sourceCodeLink: "https://github.com/AndrewKaranja/K-Player",
+        technologies: [{ id: 1, name: "Android java" }],
+        category_id: 2,
+        animation: "flip-left",
+      },
+    ];
+
+    const webProjects = projects.filter((project) => project.category_id === 1);
+    const mobileProjects = projects.filter(
+      (project) => project.category_id === 2
+    );
+    const backendProjects = projects.filter(
+      (project) => project.category_id === 3
+    );
+
+    const renderProjects = () => {
+      if (shownTab === "all") {
+        return projects;
+      } else if (shownTab === "mobile") {
+        return mobileProjects;
+      } else if (shownTab === "web") {
+        return webProjects;
+      } else {
+        return backendProjects;
+      }
+    };
+    return (
+      <div className="portfolio-main-container" id="portfolio" name="portfolio">
+        <div className="portfolio-wrapper">
+          <div className="portfolio-header">
+            <div className="home-hello-wrapper" data-aos="fade-up">
+              <div className="hello-line"></div>
+              <h3 className="hero-hello">Portfolio</h3>
+            </div>
+            <div className="portfolio-works-wrapper">
+              <div className="links-wrapper">
+                <h3 data-aos="fade-up">Some of my best works</h3>
+                <ul>
+                  <li>
+                    <div
+                      onClick={() => this.handleHowTab("all")}
+                      className={shownTab === "all" ? "active" : ""}
+                    >
+                      All
+                    </div>
+                  </li>
+                  <li onClick={() => this.handleHowTab("web")}>
+                    <div className={shownTab === "web" ? "active" : ""}>
+                      Web
+                    </div>
+                  </li>
+                  <li onClick={() => this.handleHowTab("mobile")}>
+                    <div className={shownTab === "mobile" ? "active" : ""}>
+                      Mobile{" "}
+                    </div>
+                  </li>
+                  <li onClick={() => this.handleHowTab("backend")}>
+                    <div className={shownTab === "backend" ? "active" : ""}>
+                      Backend
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div className="projects-grid-wrapper">
+                {renderProjects().map((project) => (
+                  <ProjectCard key={project.id} project={project} />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
